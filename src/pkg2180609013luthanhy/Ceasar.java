5c59da89338acf4e5b1b3ec4d6440c817f0795f6
noname77777777777777777 <technologyengineer7@gmail.com>
2024-05-20 12:00:09 +0700

    Init FrameWork

---
+++ src/pkg2180609013luthanhy/Ceasar.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg2180609013luthanhy;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Ceasar {
+    private char EncrytionChar(char value, int index){
+        if(!Character.isLetter(value)) return value;
+        
+        return (char) ((((Character.toUpperCase(value) - 'A') + index) %26 +26)%26+'A');
+    }
+    public String EncryotionCeasar(String input, int index){
+        String result = null;
+        int lenghtInput = input.length();
+        
+        for(int i = 0 ; i < lenghtInput ; i++){
+            result += EncrytionChar(input.charAt(i), index);
+        }
+        return  result;
+    }
+}
